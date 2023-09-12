4ec64956678619d92e3bd60959299ecad0b1cd2f
AntonioTrrs <antonio1959988@hotmail.com>
2023-09-11 22:34:17 -0600

    cambios minimos

--- assets/js/expresiones.js
+++ assets/js/expresiones.js
@@ -17,7 +17,187 @@ Un elemento que nos permite realizar una operacion entre dos o mas elementos.
 
 /*
 Son operadores que nos permiten tomar valores numericos como sus operandos y retornar un valor numerico unico. Corresponden a operaciones matematicas
+
+    - Suma
+    - Resta
+    - Multiplicacion (/)
+    - Division (/)
+    - Residuo / Modulo (%)
+    - Exponenciacion (**)
+    - Incremento (++)
+    - Decremento (--)
 */
+
+//Ejemplo
+//2{5*6(4) + [3-(5 - 76)]};
+//operacionCompleja = 10 + 5 / 2(5 - 6);
+
+
+//Variables
+let numeroo1 = 10;
+let numeroo2 = 2;
+let numeroo3 = "5";
+
+
+//Operaciones
+suma = numeroo1 + numeroo2;//12
+resta = numeroo1 - numeroo2; // 8 
+multiplicacion = numeroo1 * numeroo2;
+residuo = numeroo1 % numeroo2;
+exponenciacion = 10 ** 2; //Es obsoleto porque ya hay un metodo para esto
+
+
+//Impresion del valor con incremento
+//console.log("Valor del numero 1 antes del incremento: ", numeroo1);
+
+//Los valores si se pueden reasignar NOTA: Cuando usamos incrementos, el valor se reasigna (suma y reasigna en la misma operacion)
+incremento = numeroo1++; // 11
+decremento = numeroo2--; // 1
+
+//Forma extensa de un incremento
+numeroo1 = numeroo1 + 1;
+
+console.log("Valor del numero 1 despues del incremento: ", numeroo1);
+
+//Incremento de 10 en 10
+numeroo1 += 10; //20 suma y reasigna al mismo tiempo
+numeroo1 = 10 + 10
+
+
+//Operaciones en pantalla
+console.log("Suma de 10 + 2: " , suma); //Primera forma de mostrar la suma
+console.log("Suma de 10 + 2: " , numeroo1 + numeroo2); //Segunda forma
+console.log("Suma de 10 + 2: " , 10 + 2); //tercera forma
+console.log("Resta de 10 - 2", resta);
+
+
+/*
+Operadores de comparacion
+
+Operadores que nos permiten comparar dos datos y al hacerlo obtendremos un valor booleano, es decir, (true or false)
+
+    - Mayor que (>)
+    - Menor que (<)
+    - Mayor o igual que (>=)
+    - Menor o igual que (<=)
+    - igual (==)
+    - estrictamente igual (==)
+    - Diferente de (!=)
+    - estrictamente diferente (!==)
+
+
+Igual (==)
+
+Sirve para comparar que dos valores sean iguales
+
+
+
+*/
+
+let =numeroComparacion1 = 23;
+let = numeroComparacion2 = 13;
+let = numeroComparacion3 = "13";
+
+//13 == "13" //true
+//13 === "13" //false
+// 23 > 13 //true
+// 23 < 12 //false
+
+let edadFelipe = 20;
+
+if (edadFelipe >= 18){
+    console.log("Puedes ir a la fiesta");
+} else {
+    console.log("Te quedas en casa")
+}
+
+//Dos negaciones es una afirmacion
+//!vamos a comer (no vamos a comer)
+//!no me gusta java (no no me gusta llava)
+
+
+/*
+
+Operadores de asignacion
+
+Son los operadores que se usan para asignar un valor a una variable
+operador =
+
+let edad = 31;
+Let saldo = 0;
+
+*/
+
+
+/*
+Operadores logicos
+
+Nos devuelven un resultado a partir de que se cumpla o no una condicion, el resultado es booleano (true or false).
+
+    - and &&: Sirve para determinar si dos expresiones son verdaderas, 
+        Si ambas son verdaderas el resultado es verdadera.
+        Si alguna de ellas es falsa, todo el resultado es falso. 
+        Si ambas son falsas, todo es falso
+
+*/
+
+let tengoUnCrush = false;
+let miCrushMeQuiere = true;
+
+
+if (tengoUnCrush && miCrushMeQuiere){
+    console.log("Estoy Feliz :)");
+} else {
+    console.log("Haganse a un lado que me quiero automorir");
+}
+
+/*
+    Or ||: Sirve para determinar si dos expresiones son falsas.
+        Si cualquiera de las dos afirmaciones es verdadera, todo es verdadero. 
+        Si las dos son falsas, todo es falso.
+
+*/
+
+let hayChilaquilesVerdes = true;
+let hayCarneEnSuJugo = false;
+
+if (hayChilaquilesVerdes || hayCarneEnSuJugo){
+    console.log("Felipe desayuna chido");
+}  else {
+    console.log("Felipe no desayuna chido");
+}
+
+/*
+    NOT (!): Regresa lo contrario de lo que evaluamos
+
+*/
+let saldoFelipe = 20;
+let amigosQueInviten = true;
+
+let escenario1 = edadFelipe >= 18 && saldoFelipe != 0;
+
+let escenario2 = edadFelipe >= 18 && amigosQueInviten == true;
+
+if (escenario1 || escenario2){
+    console.log("chido");
+} else {
+    console.log("nochido");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Suma (+): Se utiliza para sumar dos numeros
 let resultado = 5 + 3;  // resultado contendra el valor 8
